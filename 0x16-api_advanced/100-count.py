b812a20e6ad18f037a0a553f37fa9a8ef9de81bf
rehopi <emekaokechukwu16@gmail.com>
2023-01-31 14:33:22 -0800

    Count it!

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,48 +1,38 @@
 #!/usr/bin/python3
-"""queries the Reddit API, parses the title of all hot articles, and prints a
-sorted count of given keywords(case-insensitive, delimited by spaces.
-Javascript should count as javascript, but java should not).
 """
+    Count it!
+"""
+import re
 import requests
-import operator
 
 
-def count_words(subreddit, word_list=[], word_dict={}, after=None):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
-        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-    }
-    r = requests.get('https://www.reddit.com/r/{:}/hot.json?after={:}'.format(
-        subreddit, after), headers=headers, allow_redirects=False)
-    if r.status_code == 200:
-        json = r.json()
-        data_dict = json.get('data')
-        post_list = data_dict.get('children')
-        for post in post_list:
-            post_data_dict = post.get('data')
-            for word in word_list:
-                title = post_data_dict.get('title').split()
-                title_copy = []
-                for j in title:
-                    title_copy.append(j.upper())
-                count = title_copy.count(word.upper())
-                if word_dict.get(word):
-                    word_dict[word] += count
-                else:
-                    word_dict[word] = count
-        after = data_dict.get('after')
-        if data_dict.get('after') is None:
-            sorted_list = sorted(word_dict.items(), key=operator.itemgetter(1),
-                                 reverse=True)
-            for i in range(len(sorted_list) - 1):
-                if sorted_list[i][1] == sorted_list[i+1][1]:
-                    if sorted_list[i][0] > sorted_list[i+1][0]:
-                        sorted_list[i], sorted_list[i+1] = sorted_list[
-                            i+1], sorted_list[i]
-            for item in sorted_list:
-                if item[1] > 0:
-                    print("{:}: {:}".format(item[0], item[1]))
-            return
-        return count_words(subreddit, word_list, word_dict, after)
+def count_words(subreddit, word_list):
+    storage = {word.lower(): 0 for word in word_list}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'user-agent': 'philsrequest'}
+
+    r = requests.get(url, headers=headers)
+    if (r.status_code == 404 or 'data' not in r.json()):
+        return None
     else:
-        return
+        while (1):
+            r = r.json()
+            for post in r['data']['children']:
+                tmp = post['data']['title'].split()
+                for word in tmp:
+                    if word.lower() in storage.keys():
+                        storage[word.lower()] += 1
+            after = r['data']['after']
+            if (after is None):
+                break
+            r = requests.get("{}?after={}".format(url, after), headers=headers)
+
+    storage = [(k, storage[k]) for k in
+               sorted(storage, key=storage.get, reverse=True)]
+
+    if len(storage) == 0:
+        print("")
+    else:
+        for k, v in storage:
+            if (v > 0):
+                print("{}: {:d}".format(k, v))
